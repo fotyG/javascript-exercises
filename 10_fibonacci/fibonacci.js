f8f9ccbb2d8179dd69a83adc6f6c1e666d9fa5f2
Sota Giorgadze <giorgadze@inbox.lv>
2023-05-23 09:52:26 +0300

    all tasks finished and tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  if (number < 0) return "OOPS";
+  const fibArr = [1, 1 ];
+  for (let i = 1; i < number - 1; i++ ) {
+    let num = fibArr[i] + fibArr[i-1];
+    fibArr.push(num);
+  }
+  // console.log(fibArr)
+ return fibArr[number-1]
+  
 };
 
+fibonacci(9)
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  const yearNow = new Date().getFullYear()
+  arr.forEach((person) => {
+    if(!Object.hasOwn(person, "yearOfDeath")) {
+      let age = yearNow - person.yearOfBirth
+      person.age = age;
+    } else {
+      let age = person.yearOfDeath - person.yearOfBirth;
+      person.age = age;
+    }
+  })
 
+  return (arr.sort((a, b) => b.age - a.age))[0]
 };
 
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    // yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+findTheOldest(people)
 // Do not edit below this line
 module.exports = findTheOldest;
